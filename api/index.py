efc27d989025c7d01c986b559356c6d6b0f35115
haoyan wan <haoyanwan@visitor-10-56-231-173.wifi.berkeley.edu>
2024-06-22 10:27:23 -0700

    first init

--- api/index.py
+++ api/index.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 app = Flask(__name__)
+
+load_dotenv()
 
 @app.route("/api/python")
 def hello_world():
---
+++ app/component/textbox.jsx
@@ -0,0 +1,21 @@
+function Textbox(props) {
+  return (
+    <div>
+      <label
+        htmlFor="companyName"
+        className="block font-bold text-black text-base whitespace-nowrap overflow-hidden text-ellipsis"
+      >
+        Company Name
+      </label>
+
+      <input
+        id="companyName" // Changed to match the label's htmlFor
+        type="text"
+        placeholder="Enter name" // Updated placeholder text
+        className="block mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-black focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:text-black " // Changed 'text-gray-700' to 'text-black' and adjusted dark mode text color to white
+      />
+    </div>
+  );
+}
+
+export default Textbox;
